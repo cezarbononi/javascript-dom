f41e28a38f77b70f0c5f54eaaf68cab7f07a1da9
Cezar Bononi <cezarbononi@gmail.com>
2021-11-30 23:27:19 -0300

    adding private decode

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,22 +1,40 @@
-const criarTarefa = (evento) => {
-    evento.preventDefault()
+(() => {
+    const criarTarefa = (evento) => {
+        evento.preventDefault()
 
-    const lista = document.querySelector('[data-list]')
+        const lista = document.querySelector('[data-list]')
 
-    const input = document.querySelector('[data-form-input]')
-    const valor = input.value
+        const input = document.querySelector('[data-form-input]')
+        const valor = input.value
 
-    const tarefa = document.createElement('li')
-    tarefa.classList.add('task')
-    const conteudo = `<p class="content">${valor}</p>`
+        const tarefa = document.createElement('li')
+        tarefa.classList.add('task')
+        const conteudo = `<p class="content">${valor}</p>`
 
-    tarefa.innerHTML = conteudo
+        tarefa.innerHTML = conteudo
 
-    lista.appendChild(tarefa)
-    input.value = " "
+        tarefa.appendChild(BotaoConclui())
+        lista.appendChild(tarefa)
+        input.value = " "
+    }
+
+    const novaTarefa = document.querySelector('[data-form-button]')
+
+    novaTarefa.addEventListener('click', criarTarefa)
+
+    const BotaoConclui = () => {
+        const botaoConclui = document.createElement('button')
+        botaoConclui.classList.add('check-button')
+        botaoConclui.innerText = 'concluir'
+        botaoConclui.addEventListener('click', concluirTarefa)
+
+        return botaoConclui
+    }
+
+    const concluirTarefa = (evento) => {
+        const botaoConclui = evento.target
+        const tarefaCompleta = botaoConclui.parentElement
+        tarefaCompleta.classList.toggle('done')
+    }
 }
-
-const novaTarefa = document.querySelector('[data-form-button]')
-
-novaTarefa.addEventListener('click', criarTarefa)
-
+)()
