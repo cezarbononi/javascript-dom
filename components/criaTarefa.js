12ff6386d17979b02ebb9bedaf7b8041be568669
Cezar Bononi <cezarbononi@gmail.com>
2021-12-06 12:07:02 -0300

    alterando valores, filtrando tarefas

--- components/criaTarefa.js
+++ components/criaTarefa.js
@@ -1,3 +1,4 @@
+import { carregaTarefa } from './carregaTarefa.js'
 import BotaoConclui from './concluiTarefa.js'
 import BotaoDeleta from './deletaTarefa.js'
 
@@ -6,12 +7,12 @@ export const handleNovoItem = (evento) => {
 
     const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
 
-    const lista = document.querySelector('[data-list]')
     const input = document.querySelector('[data-form-input]')
     const valor = input.value
 
     const calendario = document.querySelector('[data-form-date]')
     const data = moment(calendario.value)
+
     const dataFormatada = data.format('DD/MM/YYYY')
 
     const dados = {
@@ -21,11 +22,10 @@ export const handleNovoItem = (evento) => {
 
     const tarefasAtualizadas = [...tarefas, dados]
 
-    const criaTarefa = Tarefa(dados)
-
     localStorage.setItem('tarefas', JSON.stringify(tarefasAtualizadas))
-    lista.appendChild(criaTarefa)
+
     input.value = " "
+    carregaTarefa()
 }
 
 export const Tarefa = ({ valor, dataFormatada }) => {
